3460c0e954cf5d95340dc9870ecf8224373dc1d8
Shresth Singh <ss1661@duke.edu>
2026-01-01 14:37:32 -0500

    Style philosophy markdown to match site

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 # Convert philosophy markdown files to HTML
-pandoc philosophy/README.md -o philosophy/README.html
-pandoc philosophy/presocratics.md -o philosophy/presocratics.html
-pandoc philosophy/socratic-dialogues.md -o philosophy/socratic-dialogues.html
+pandoc philosophy/README.md \
+  --template pandoc-template.html \
+  -o philosophy/README.html
+
+pandoc philosophy/presocratics.md \
+  --template pandoc-template.html \
+  -o philosophy/presocratics.html
+
+pandoc philosophy/socratic-dialogues.md \
+  --template pandoc-template.html \
+  -o philosophy/socratic-dialogues.html
+
